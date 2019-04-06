00982ffd7fd44d925eec2130eee34f2d1570df3c
Your Name <you@example.com>
2019-04-06 00:29:52 -0400

    Entity  FrAamework instalado

---
+++ venta producto y clientes/venta producto y clientes/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace venta_producto_y_clientes.Models
+{
+    public class DataContext:DbContext
+    {
+    }
+}
---
+++ venta producto y clientes/venta producto y clientes/Models/Ventas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace venta_producto_y_clientes.Models
+{
+    public class Ventas
+    {
+        int VentasID;
+        DateTime fecha;
+        int productoID;
+        int clienteID;
+
+    }
+}
